ffa0ec29e56cc64dcc2ace9748e3579dd2a3ab88
Raid+ <82845008+Raidisuntaken@users.noreply.github.com>
2021-08-29 16:46:56 +0300

    Create openwindow.py

---
+++ openwindow.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter
+
+
+root = Tk()
+root.title("Your Window Name")
+
+
+
+root.geometry('1080x600')
+root.resizable(FALSE,FALSE)  #not enlargement window
+
+root.mainloop() #close window
